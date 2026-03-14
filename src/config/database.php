939dfb3ec975233ad9d07597fb2ee67fa1965a2a
John Saina <jrock8371@gmail.com>
2026-03-14 16:53:02 +0300

    mise a jour database.php

--- src/config/database.php
+++ src/config/database.php
@@ -1,19 +1,19 @@
 <?php
 
-return [
-    'host' => 'localhost',
-    'dbname' => 'orientation',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8mb4',
-    'jwt_secret' => 'ORIENTATION_SECRET_2026_CHANGE_ME',
-];
-
 // return [
 //     'host' => 'localhost',
-//     'dbname' => 'bambaray_orientation',
-//     'username' => 'bambaray_bambaray',
-//     'password' => ')t%Bf@QAJ40!kHBu',
+//     'dbname' => 'orientation',
+//     'username' => 'root',
+//     'password' => '',
 //     'charset' => 'utf8mb4',
 //     'jwt_secret' => 'ORIENTATION_SECRET_2026_CHANGE_ME',
 // ];
+
+return [
+    'host' => 'localhost',
+    'dbname' => 'bambaray_orientation',
+    'username' => 'bambaray_bambaray',
+    'password' => ')t%Bf@QAJ40!kHBu',
+    'charset' => 'utf8mb4',
+    'jwt_secret' => 'ORIENTATION_SECRET_2026_CHANGE_ME',
+];
