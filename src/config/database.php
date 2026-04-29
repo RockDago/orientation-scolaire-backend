f98528c07fa8813fe26fe88c897a803d3d604c81
John Saina <jrock8371@gmail.com>
2026-04-29 21:30:13 +0300

    update config/database.php

--- src/config/database.php
+++ src/config/database.php
@@ -24,8 +24,7 @@ return [
     'password' => ')t%Bf@QAJ40!kHBu',
     'charset' => 'utf8mb4',
     'jwt_secret' => 'ORIENTATION_SECRET_2026_CHANGE_ME',
-'app_debug' => false,
-    'allowed_origins' => [
+'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost',
